1e66d9253e60f5630b5e60982144a6f58b6b875f
Abdul Rafay <abdulrafayaslam.007@gmail.com>
2024-12-03 23:19:59 +0500

    add data table ui

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -7,6 +7,8 @@ import { useGetTasks } from "@/features/tasks/api/use-get-tasks";
 import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 import { DataFilter } from "@/features/tasks/components/data-filters";
 import { useTaskFilters } from "@/features/tasks/hooks/use-task-filters";
+import { DataTable } from "@/features/tasks/components/data-table";
+import { columns } from "@/features/tasks/components/columns";
 
 import { DottedSeparator } from "@/components/dotted-separator";
 import {
@@ -91,7 +93,7 @@ export const TaskViewSwitcher = () => {
             ) : (
                <>
                   <TabsContent value="table" className="mt-0">
-                     {JSON.stringify(tasks)}
+                     <DataTable columns={columns} data={tasks?.documents ?? []} />
                   </TabsContent>
                   <TabsContent value="kanban" className="mt-0">
                      {JSON.stringify(tasks)}
--- src/features/tasks/server/route.ts
+++ src/features/tasks/server/route.ts
@@ -7,7 +7,7 @@ import { DATABASE_ID, MEMBERS_ID, PROJECTS_ID, TASKS_ID } from "@/config";
 import { getMember } from "@/features/members/utils";
 import { Project } from "@/features/projects/types";
 import { createTaskSchema } from "@/features/tasks/schema";
-import { TaskStatus } from "@/features/tasks/types";
+import { Task, TaskStatus } from "@/features/tasks/types";
 import { createAdminClient } from "@/lib/appwrite";
 import { sessionMiddleware } from "@/lib/session-middleware";
 
@@ -64,7 +64,7 @@ const app = new Hono()
          query.push(Query.search("name", search));
       };
 
-      const tasks = await databases.listDocuments(
+      const tasks = await databases.listDocuments<Task>(
          DATABASE_ID,
          TASKS_ID,
          query,
